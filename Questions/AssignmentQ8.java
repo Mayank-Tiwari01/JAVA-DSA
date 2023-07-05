7669b245d8a053d78541c07d3e039cd96b285e42
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-05 00:31:26 +0000

    as

---
+++ Questions/AssignmentQ8.java
@@ -0,0 +1,25 @@
+//To find out whether the given String is Palindrome or not.
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class AssignmentQ8 {
+    public static void main(String[] args){
+        
+        System.out.println("Enter a string: ");
+
+        Scanner input = new Scanner(System.in);
+        String s = new String();
+
+        s = input.nextLine();
+        int lenght = s.length();
+
+        Array a[] = new Array();
+        
+
+        for (int i = 0; i < lenght; i++){
+
+        }
+
+    }
+    
+}
