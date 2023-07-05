7cfe94afedbd5b26aacbfd94317d91bdc369a8d9
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-05 01:48:19 +0000

    z

--- Questions/AssignmentQ8.java
+++ Questions/AssignmentQ8.java
@@ -1,37 +1,115 @@
 //To find out whether the given String is Palindrome or not.
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class AssignmentQ8 {
-    public static void main(String[] args){
-        
-        System.out.println("Enter a string: ");
-
-        Scanner input = new Scanner(System.in);
-        String s = new String();
-
-        s = input.nextLine();
+    public static boolean isPalindrome(String s) {
         int length = s.length();
 
         char[] array = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = s.charAt(i);
+            // array = s.toCharArray();
+        }
+
+        char[] reverse = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            reverse[i] = array[length - 1 - i];
+        }
+
+        for (int j = 0; j < length; j++) {
+            if (array[j] != reverse[j]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = input.nextLine();
+
+        if (isPalindrome(s)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+    
+}
+
+
+
+
+
+
+// import java.util.Scanner;
+
+// public class AssignmentQ8 {
+//     public static boolean IsPalindrome(String s){
+        
+//         System.out.println("Enter a string: ");
+
+//         Scanner input = new Scanner(System.in);
+        
+
+//         s = input.nextLine();
+//         int length = s.length();
+
+//         char[] array = new char[length];
        
         
 
-        for (int i = 0; i < length; i++){
-            //  array = s.toCharArray();
-            array[i] = s.charAt(i);
+//         for (int i = 0; i < length; i++){
+//             //  array = s.toCharArray();
+//             array[i] = s.charAt(i);
             
-        }
+//         }
 
-        char[] reverse;
-        reverse = new char[length];
+//         char[] reverse;
+//         reverse = new char[length];
 
-        for (int i = 0; i < length; i++) {
-        reverse[i] = array[length - 1 - i];
-        }
+//         for (int i = 0; i < length; i++) {
+//         reverse[i] = array[length - 1 - i];
+//         }
 
-        for (int )
+//         for (int j = 0; j < length; j++) {
 
-    }    
+//             if ( array[j] == reverse[j])
+//             {
+//                 return true; 
+//             }
 
-}
+//             else{
+//                 return false;
+//             }
+
+//         }
+
+//         return true;
+       
+
+       
+
+//     }
+    
+//     public static void main(String[] args){
+        
+//         if(IsPalindrome(null)){
+//             System.out.println("The string is a palindrome.");
+
+//         }
+
+//         else{
+//             System.out.println("The string is not a palindrome.");
+     
+//         }
+
+//     }
+    
+    
+
+// }
