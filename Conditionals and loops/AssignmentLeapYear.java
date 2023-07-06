3a106c8bab171cb6a42193fd6cf0a6185a58b0ee
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-06 19:59:34 +0000

    a

--- Conditionals and loops/AssignmentLeapYear.java
+++ Conditionals and loops/AssignmentLeapYear.java
@@ -3,7 +3,33 @@ import java.util.Scanner;
 public class AssignmentLeapYear {
     public static void main(String[] args) {
         
-        Scanner = new Scanner(system.in);
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a year");
+        int year = input.nextInt();
+
+        if(isLeapYear (year)) {
+            System.out.println("It is a leap year!");
+        }
+
+        else{
+            System.out.println("not a leap year.");
+        }
+
+        input.close();
+
     }
+
+    public static boolean isLeapYear (int year){
+
+        if ( year % 4 == 0){
+            if ( year % 100 == 0 && year % 400 != 0){
+                return false;
+            }
+            return true;
+        }
+        return false;
+
+    }
+
+
 }
