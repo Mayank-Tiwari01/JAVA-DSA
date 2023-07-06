1c7ac1b5bd42c974fcc67051630a50286d07870e
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-06 00:19:52 +0000

    c

---
+++ Conditionals and loops/AssignmentPandC.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class AssignmentPandC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter n and r: ");
+        int n = input.nextInt();
+        int r = input.nextInt();
+
+        int rfactorial;
+
+        while (n !=0){
+            rfactorial = n(n-1);
+        }
+        
+
+        System.out.println("Select 0 for Permutation and 1 for Combination");
+        int choice = input.nextInt();
+
+        if(choice == 0){
+            //
+        }
+
+        if else( choice == 1){
+            //
+        }
+
+        else{
+            System.out.println("Please select 0 or 1");
+        }
+
+    }
+
+    private static int n(int i) {
+        return 0;
+    }
+    
+}
