d1d538b00dc4fed2aedfa99e7a4d56c1849eedf2
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-06 01:05:50 +0000

    gpt

--- Conditionals and loops/AssignmentPandC.java
+++ Conditionals and loops/AssignmentPandC.java
@@ -50,3 +50,50 @@ public class AssignmentPandC {
     }
 
 }
+
+// import java.util.Scanner;
+
+// public class AssignmentPandC {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+
+//         System.out.print("Enter n and r: ");
+//         int n = input.nextInt();
+//         int r = input.nextInt();
+
+//         if (n < r) {
+//             System.out.println("Invalid input. n should be greater than or equal to r.");
+//             input.close();
+//             return;
+//         }
+
+//         int nMinusRFactorial = 1;
+//         int nFactorial = 1;
+//         int rFactorial = 1;
+
+//         for (int i = 1; i <= n; i++) {
+//             nFactorial *= i;
+//             if (i <= r) {
+//                 rFactorial *= i;
+//             }
+//             if (i <= n - r) {
+//                 nMinusRFactorial *= i;
+//             }
+//         }
+
+//         System.out.println("Select 0 for Permutation and 1 for Combination");
+//         int choice = input.nextInt();
+
+//         if (choice == 0) {
+//             int permutation = nFactorial / nMinusRFactorial;
+//             System.out.println("nPr: " + permutation);
+//         } else if (choice == 1) {
+//             int combination = nFactorial / (nMinusRFactorial * rFactorial);
+//             System.out.println("nCr: " + combination);
+//         } else {
+//             System.out.println("Invalid choice. Please select 0 or 1.");
+//         }
+
+//         input.close();
+//     }
+// }
