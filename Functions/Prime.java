5011c5304c444f3a46dec00a08cf96ae8fcbada8
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-07 03:11:23 +0000

    z

--- Functions/Prime.java
+++ Functions/Prime.java
@@ -6,6 +6,30 @@ public class Prime {
         System.out.print(" Enter a number: ");
 
         Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        boolean prime = prime(num);
+        System.out.println(prime(num));
+
+        input.close();
+
+    }
+
+    static boolean prime(int num){
         
+        if(num ==1){
+            return false;
+        }
+
+        int n = 2;
+        while(n * n <= num){
+            if(num % n == 0){
+                return false;
+            }
+            n++;
+
+
+        }
+        return n * n > num;
     }
 }
