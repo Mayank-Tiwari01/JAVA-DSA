7b2ab19e71fd87f96b673aeff0d803af79eec7fd
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-07 02:44:45 +0000

    d

--- Functions/EvenOdd.java
+++ Functions/EvenOdd.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class EvenOdd {
     public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+        System.out.println(evenOdd(num));
         
-        
+    }
+    static boolean evenOdd(int num){
+
+        if(num % 2 == 0){
+            return true;
+        }
+
+        return false;
+
     }
 }
