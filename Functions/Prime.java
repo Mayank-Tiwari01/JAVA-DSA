c9a29b35ae2cfc3ac92763a2863eb69c938d13ba
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-07 02:51:58 +0000

    s

---
+++ Functions/Prime.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        
+        System.out.print(" Enter a number: ");
+
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
