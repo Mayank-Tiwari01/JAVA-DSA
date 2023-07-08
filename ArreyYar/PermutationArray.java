6595b02ec4212cec54a5e0d426561d3ab318a3a4
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-08 01:31:07 +0000

    x

---
+++ ArreyYar/PermutationArray.java
@@ -0,0 +1,17 @@
+package ArreyYar;
+
+import java.util.Arrays;
+
+public class PermutationArray {
+    public static void main(String[] args) {
+
+        int[] nums ={0,2,1,5,3,4};
+        int[] ans = new int[6];
+
+        for ( int i = 0; i < 6; i++ ){
+            ans[i] = nums[nums[i]];
+        }
+        System.out.println(Arrays.toString(ans));
+
+    }
+}
