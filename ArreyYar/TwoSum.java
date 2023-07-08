55019b02d02603ddd0465eea8c2df87d3f78ebd8
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-08 03:35:47 +0000

    d

--- ArreyYar/TwoSum.java
+++ ArreyYar/TwoSum.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15,2};
-        int target = 4;
+        int[] nums = {2,5,5,11};
+        int target = 10;
         int[] sol = new int[2];
 
-        for( int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
-                if (nums[i] + nums[j] == target){
-                    sol[i] = nums[i];
-                    sol[j] = nums[j];
+        for( int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length ; j++){
+                if (nums[i] + nums[j] == target && i != j){
+                    sol[1] = i;
+                    sol[0] = j;                    
                 }
             }
         }
         System.out.println(Arrays.toString(sol));
+        
     }
 }
