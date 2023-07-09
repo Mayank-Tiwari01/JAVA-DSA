ee1c5fc1dd3136607da1dd18ac3c7c3fa1cfccfe
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-09 19:04:14 +0000

    x

--- ArreyYar/Target.java
+++ ArreyYar/Target.java
@@ -1,21 +1,47 @@
 package ArreyYar;
 
+// import java.util.Arrays;
+
+// public class Target {
+//     public static void main(String[] args) {
+//         int[] nums = {0, 1, 2, 3, 4};
+//         int[] index = {0, 1, 2, 2, 1};
+//         int[] targetArray = new int[5];
+
+//         for (int i = 0; i < 5; i++) {
+//             if (i >= index[i]) {
+//                 targetArray[i] = nums[i];
+//             } else {
+//                 targetArray[index[i]] = nums[i];
+//             }
+//         }
+//         System.out.println(Arrays.toString(targetArray));
+//     }
+// }
+
+
+
 import java.util.Arrays;
 
 public class Target {
     public static void main(String[] args) {
-        int[] nums = {0, 1, 2, 3, 4};
-        int[] index = {0, 1, 2, 2, 1};
-        int[] targetArray = new int[5];
+        int[] nums = {0, 1, 0};
+        int[] index = {0, 1, 0};
+        int[] targetArray = new int[nums.length];
+        int length = nums.length;
 
-        for(int i = 0, j= 0; i < 5; i++){
-            if( i >= index[i]){
-                targetArray[i] = nums[j++];
-            }
-            else{
-                targetArray[index[i]] = nums[i];
-            }
-        }
+        // for (int i = 0; i < length; i++) {
+
+        //     if (targetArray[index[i]] != 0) {
+        //         // Shift elements to the right if the target position is already occupied
+        //         for (int j = length - 1; j > index[i]; j--) {
+        //             targetArray[j] = targetArray[j - 1];
+        //         }
+        //     }
+        //     targetArray[index[i]] = nums[i];
+        // }
+
         System.out.println(Arrays.toString(targetArray));
-    } 
+    }
 }
+
