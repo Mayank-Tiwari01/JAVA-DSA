3aa079d5a9f560976511bfa75e92a4cc649e2723
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-10 20:34:55 +0000

    x

--- ArreyYar/Unique.java
+++ ArreyYar/Unique.java
@@ -2,20 +2,31 @@ package ArreyYar;
 import java.util.Arrays;
 public class Unique {
     public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {1,1,2};
         int length = nums.length;
         int[] uniqueArrayFrequency = new int[length];
         int freq = 0;
 
         for (int i = 0; i < length; i++){
-            for (int j = 1; j < length; j++){
+            for (int j = 0; j < length ; j++){
                 if(nums[i] == nums[j]){
                     freq++;
                 }
             }
+            uniqueArrayFrequency[i] = freq;
+            freq = 0;
         }
 
-        System.out.println(Arrays.toString(uniqueArray));  
+        int numOfUnique = 0;
+
+        for (int i = 0; i < length; i++){
+            if(uniqueArrayFrequency[i] == 1){
+                numOfUnique++;
+            }
+        }
+
+        System.out.println(Arrays.toString(uniqueArrayFrequency));
+        System.out.println(numOfUnique);
     }
 
 }
