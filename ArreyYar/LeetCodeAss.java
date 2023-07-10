86638533c736eb3539b5731919a205cce8cdbebf
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-10 23:43:12 +0000

    x

--- ArreyYar/LeetCodeAss.java
+++ ArreyYar/LeetCodeAss.java
@@ -1,5 +1,24 @@
 package ArreyYar;
 
+import java.util.Arrays;
+
 public class LeetCodeAss {
-    int[] nums = 
+    public static void main(String[] args) {
+
+    int[] nums = {0,0,1,1,1,2,2,3,3,4};
+    int length = nums.length;
+    int i = 0; 
+  
+
+    for (int j = 1; j < length; j++){
+        if(nums[j] != nums[i]){
+           nums[i+1] = nums[j];
+            i++;
+        }
+    }
+    System.out.println(Arrays.toString(nums));
+    System.out.println(i+1);       
+
+  }
 }
+
