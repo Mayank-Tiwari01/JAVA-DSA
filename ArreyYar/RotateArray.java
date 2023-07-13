699b8628669c875cc84a32a13af122d626ee2888
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-13 03:39:52 +0000

    c

--- ArreyYar/RotateArray.java
+++ ArreyYar/RotateArray.java
@@ -5,24 +5,27 @@ import java.util.Arrays;
 public class RotateArray {
    public static void main(String[] args) {
     
-    int [] nums = {-1,-100,3,99};
-    int k = 2;
+    int [] nums = {9,2,2,2,2,2,1,1,1,1,1,1,1,1,32,1,33,21};
+    int k = 3;
     int length = nums.length;
+    k = k % length; 
     int [] ans = new int[length];
     int j = 0;
     
-    for (int i = k; i < length; i++){
+    for (int i = length - k; i < length; i++){
         ans[j] = nums[i];
         j++;
     }
 
     System.out.println(Arrays.toString(ans));
-    int l = k;
-
-    for(int i = 0; i < k+1; i++){
-        ans[l] = nums[i];
+    
+    int l = 0;
+    for(int i = k; i < length ; i++){
+        ans[i] = nums[l];
         l++;
+       
     }
+
     System.out.println(Arrays.toString(ans));
 
    } 
