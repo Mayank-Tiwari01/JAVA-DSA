74e4fbfd01a3d5e9ee6a930cb57f1bda10cafc7f
Mayank Tiwari <mayank.tiwari0106@gmail.com>
2023-07-18 04:54:55 +0000

    w

---
+++ BS/Ceilling.java
@@ -0,0 +1,39 @@
+package BS;
+
+public class Ceilling {
+    public static void main(String args[]){
+        int[] arr = {2, 5, 8, 11, 14, 17, 20};
+        int target = 22;
+
+        int ans = CeillingNum(arr,target);
+        System.out.println(ans);
+
+    }
+
+    static int CeillingNum(int arr[], int target){
+        int start = 0;
+        int end = arr.length - 1;
+        if(target > arr[arr.length - 1]){
+            return - 1;
+        }
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target){
+                return arr[mid];
+            }
+
+            if(arr[mid] < target){
+                start = mid + 1;
+            }
+
+            else {
+                end = mid - 1;
+            }
+
+        }
+        
+        return arr[start];
+    }
+}
