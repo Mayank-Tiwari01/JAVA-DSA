ed3dfe74e582e9bbd3060c18a247275e1e596fb7
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-07-20 09:22:43 +0530

    hwllo

--- BS/FirstandLastPart2.java
+++ BS/FirstandLastPart2.java
@@ -2,6 +2,8 @@ package BS;
 
 public class FirstandLastPart2 {
     public static void main(String[] args) {
-
+        int[] nums = {1,2,3,4,4,4,43,32,4,4,1};
+        int target = 4;
+        
     }
 }
