8d6e2e06253d58e9481eed0d32491244eb464706
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-07-20 11:26:42 +0530

    hwllo

--- BS/CorrectPalindome.java
+++ BS/CorrectPalindome.java
@@ -1,5 +1,25 @@
 package BS;
 
 public class CorrectPalindome {
-    p
+    public static void main(String[] args) {
+        int x = 123123;
+        if(isPalindrome(x)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("false");
+        }
+
+    }
+    static boolean isPalindrome(int x) {
+        if (x == 0 || x < 0 || x % 10 == 0) {
+            return false;
+        }
+        int temp = 0;
+        while(temp < x){
+            temp = temp * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == temp || x == temp / 10);
+    }
 }
