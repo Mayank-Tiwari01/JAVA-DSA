5857d80383bbb64ecb7a5762000874e068eca3d7
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-07-23 13:08:43 +0530

    hwllo

--- BS/RotatedBSPartOne.java
+++ BS/RotatedBSPartOne.java
@@ -2,8 +2,9 @@ package BS;
 
 public class RotatedBSPartOne {
     public static void main(String[] args) {
-        int[] nums = {5,1,3};
-        int target = 5;
+        int[] nums = {0,1,2,3,4,5,6,7,8,9,10};
+        int target = 10;
+        System.out.println(BS(nums, target));
     }
     static int BS(int[] nums, int target){
         int start = 0;
@@ -14,15 +15,25 @@ public class RotatedBSPartOne {
             if(nums[mid] == target){
                 return mid;
             }
-            if(nums[mid] > target && nums[0] < nums[mid]){
-                end = mid -1;
+            //if the left part is sorted.
+            if(nums[mid] > nums[0] ){
+                if(target < nums[mid] && nums[0] < target){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
             }
-            else if(nums[mid] > target && nums[0] > nums[mid]){
-
-            }
-            else if(nums[mid] < target && nums[mid] < nums[nums.length - 1]){
-                start = mid + 1;
+            //or the right part of the array is sorted
+            else{
+                    if(target > nums[mid] && target < nums[nums.length - 1]){
+                        start = mid + 1;
+                    }
+                    else{
+                        end = mid - 1;
+                    }
             }
         }
+        return -1;
     }
 }
