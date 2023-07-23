78c201ad257a3c8c67938a14c94ef1ed817adeef
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-07-23 15:49:05 +0530

    hwllo

--- BS/SearchRotatedSortedArrayII.java
+++ BS/SearchRotatedSortedArrayII.java
@@ -2,43 +2,44 @@ package BS;
 
 public class SearchRotatedSortedArrayII {
     public static void main(String[] args) {
-        int[] nums = {1, 0, 1, 1, 1};
-        int target = 0;
-        boolean ans = duplicateInRotatedSorted(nums, target);
+        int[] nums = {1,1,1,1,1,1,1,1,1,13,1,1,1,1,1,1,1,1,1,1,1,1};
+        int target = 13;
+        boolean ans = DuplicateInRotatedSorted(nums, target);
         System.out.println(ans);
     }
-
-    static boolean duplicateInRotatedSorted(int[] nums, int target) {
+    static boolean DuplicateInRotatedSorted(int[]nums, int target){
         int start = 0;
         int length = nums.length;
         int end = length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
 
-            if (nums[mid] == target) {
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
                 return true;
             }
-
-            if (nums[0] == nums[mid] && nums[mid] == nums[length - 1]) {
-                // Handle duplicates at both ends
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
                 start++;
                 end--;
-            } else if (nums[0] <= nums[mid]) {
-                if (target >= nums[0] && target < nums[mid]) {
+                continue;
+            }
+            if(nums[start] <= nums[mid]){
+                if(target <= nums[mid] && target >= nums[start]){
                     end = mid - 1;
-                } else {
+                }
+                else{
                     start = mid + 1;
                 }
-            } else {
-                if (target > nums[mid] && target <= nums[length - 1]) {
+            }
+            else{
+                if(target >= nums[mid] && target <= nums[end]){
                     start = mid + 1;
-                } else {
+                }
+                else{
                     end = mid - 1;
                 }
             }
         }
-
         return false;
     }
 }
