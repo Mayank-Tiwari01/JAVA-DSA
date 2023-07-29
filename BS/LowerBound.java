6f39a5a13df1268506486f335c7fa34e0aa5171f
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-07-29 12:59:40 +0530

    hwllo

---
+++ BS/LowerBound.java
@@ -0,0 +1,27 @@
+package BS;
+
+public class LowerBound {
+    public static void main(String[] args){
+        int[] arr = {1,2,2,3};
+        int k = 4;
+        System.out.println(ans(arr,k));
+    }
+    static int ans(int[] arr, int k){
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = arr.length;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] >= k){
+                 ans = mid;
+                 //there can be repetition
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
