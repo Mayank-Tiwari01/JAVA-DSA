450487879898a94d67b326998ef268b244466f1b
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-07-29 14:13:30 +0530

    hwllo

--- BS/LowerBound.java
+++ BS/LowerBound.java
@@ -2,8 +2,8 @@ package BS;
 //the brute force solution of this problem would be a simple linear search.
 public class LowerBound {
     public static void main(String[] args){
-        int[] arr = {1,2,2,3,4,5,6,7,8,9,9,9,10,10,11};
-        int k = 23;
+        int[] arr = {1,2,2,3,4,5,6,7,8,9,9,9,10,10,12};
+        int k =11;
         System.out.println(ans(arr,k));
     }
     static int ans(int[] arr, int k){
