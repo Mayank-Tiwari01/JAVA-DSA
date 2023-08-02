f207af8df9e76a0905f8b2a7566b32e1cf931dad
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-02 11:24:28 +0530

    hwllo

--- BS/KokoEatingBananas.java
+++ BS/KokoEatingBananas.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class KokoEatingBananas {
     public static void main(String[] args) {
-        int[] piles = {3, 6, 11, 7};
-        int h = 8;
+        int[] piles = {30,11,23,4,20};
+        int h = 5;
         // Sorting the piles array
         Sort(piles);
         // Displaying the sorted array
@@ -32,7 +32,7 @@ public class KokoEatingBananas {
             }
             else{
                 sol = mid;
-                end = mid;
+                end = mid - 1;
             }
         }
         return sol;
