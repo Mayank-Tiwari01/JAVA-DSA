2f876787438732d7605396653c10c58b80139bcc
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-06 01:35:02 +0530

    hwllo

---
+++ BS/FairCandySwap.java
@@ -0,0 +1,29 @@
+package BS;
+
+public class FairCandySwap {
+    public static void main(String[] args){
+        int[] aliceSizes = {1,2};
+        int[] bobSizes = {2,3};
+    }
+    static int FindMaxAlice(int[] aliceSizes){
+        int max = Integer.MIN_VALUE;
+        for(int nums : aliceSizes) {
+            if (nums > max) {
+                max = nums;
+            }
+        }
+        return max;
+    }
+    static int FindMaxBob(int[] bobSize){
+        int max = Integer.MIN_VALUE;
+        for(int nums : bobSize) {
+            if (nums > max) {
+                max = nums;
+            }
+        }
+        return max;
+    }
+    static int[] FairDistribution(int[] aliceSizes, int[] bobSizes){
+        int
+    }
+}
