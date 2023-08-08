0551fc7d49af60093eaffb111326da29682c0914
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-08 21:03:10 +0530

    hwllo

--- BS/KthMissingPositiveNumberUsingBS.java
+++ BS/KthMissingPositiveNumberUsingBS.java
@@ -6,33 +6,41 @@ public class KthMissingPositiveNumberUsingBS {
         int k = 1;
         System.out.println(binarySearch(arr, k));
     }
-    static int binarySearch(int[] arr, int k){
+    static int binarySearch(int[] arr, int k) {
         int start = 0;
         int end = arr.length - 1;
-        if((arr[end] - 1) - (end) < k){
-            int x = (arr[end] - 1) - (end);
-            while(x < k){
-                arr[end]++;
+        int lastElement = arr.length - 1;
+        int firstElement = 0;
+        if ((arr[firstElement] - 1) - firstElement >= k) {
+            return k;
+        }
+
+        if ((arr[lastElement] - 1) - (lastElement) < k) {
+            int x = (arr[lastElement] - 1) - (lastElement);
+            while (x < k) {
+                arr[lastElement]++;
                 x++;
             }
-            return arr[end];
-        }while(start <= end){
-            int mid = start + (end - start) / 2;
-            if((arr[mid] - 1) - (mid) < k && (arr[mid + 1] - 1) - (mid + 1) >= k && (mid + 1) < arr.length && mid < arr.length){
-                int x = (arr[mid] - 1) - mid;
-                while(x < k){
-                    arr[mid]++;
-                    x++;
-                }
-                return arr[mid];
-            }
-            if((arr[mid] - 1) - (mid) < k && (arr[mid + 1] - 1) - (mid + 1) < k && (mid + 1) < arr.length && mid < arr.length){
-                start = mid + 1;
-            }
-            if((arr[mid] - 1) - (mid) >= k && (arr[mid + 1] - 1) - (mid + 1) >= k && (mid + 1) < arr.length && mid < arr.length){
-                end = mid - 1;
-            }
+            return arr[lastElement];
         }
-        return -1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if ((arr[mid] - 1) - (mid) < k && (arr[mid + 1] - 1) - (mid + 1) >= k && (mid + 1) < arr.length && mid < arr.length) {
+                    int x = (arr[mid] - 1) - mid;
+                    while (x < k) {
+                        arr[mid]++;
+                        x++;
+                    }
+                    return arr[mid];
+                }
+                if ((arr[mid] - 1) - (mid) < k && (arr[mid + 1] - 1) - (mid + 1) < k && (mid + 1) < arr.length && mid < arr.length) {
+                    start = mid + 1;
+                }
+                if ((arr[mid] - 1) - (mid) >= k && (arr[mid + 1] - 1) - (mid + 1) >= k && (mid + 1) < arr.length && mid < arr.length) {
+                    end = mid - 1;
+                }
+            }
+            return -1;
+        }
     }
-}
