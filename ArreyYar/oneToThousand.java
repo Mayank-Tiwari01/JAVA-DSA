d6d92bb2d68e978c648f75d07aeeb7276857f7f6
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-08 23:13:42 +0530

    hwllo

---
+++ ArreyYar/oneToThousand.java
@@ -0,0 +1,9 @@
+package ArreyYar;
+
+public class oneToThousand {
+    public static void main(String[] args) {
+        for(int i = 1; i < 1001; i++){
+            System.out.print(i + ",");
+        }
+    }
+}
