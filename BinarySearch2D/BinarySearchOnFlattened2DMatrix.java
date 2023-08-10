0fe4d22608c48cb020dec7834a9bad24d3b1d3d8
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-10 17:49:42 +0530

    hwllo

--- BinarySearch2D/BinarySearchOnFlattened2DMatrix.java
+++ BinarySearch2D/BinarySearchOnFlattened2DMatrix.java
@@ -1,68 +1,49 @@
 package BinarySearch2D;
-//this is also of fixded col length
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class BinarySearchOnFlattened2DMatrix {
     public static void main(String[] args) {
         int[][] sortedMatrix = {
-                {1, 3, 5},
-                {7, 8},
-                {10, 12, 14, 16},
-                {18, 19, 20}
+                {1,2,3,4,5,6},
+                {7,8,9,10,11,12},
+                {13,14,15,16,17,18,19}
         };
-
         System.out.println("Enter Element you want to search: ");
         Scanner input = new Scanner(System.in);
         int target = input.nextInt();
-
         int[] ans = BinarySearchOnFlattenedMatrix(sortedMatrix, target);
-
-        if (ans[0] == -1) {
+        if(ans[0] == -1){
             System.out.println("Element not present in the 2D array.");
-        } else {
-            System.out.println("The Element: " + target + " is present at: " + Arrays.toString(ans) + " index");
+        }
+        else{
+            System.out.println("The Element: " +target+ " is present at: " + Arrays.toString(ans)+ " index");
         }
     }
-
-    static int[] BinarySearchOnFlattenedMatrix(int[][] sortedMatrix, int target) {
+    static int[] BinarySearchOnFlattenedMatrix(int[][] sortedMatrix, int target){
         int nRows = sortedMatrix.length;
+        int nCols = sortedMatrix[0].length;
+        if(nCols == 0){
+            return new int[]{-1,-1};
+        }
         int low = 0;
-        int high = nRows - 1;
+        int high = nRows * nCols - 1;
 
-        while (low <= high) {
+        while(low <= high){
             int mid = low + (high - low) / 2;
-
-            if (sortedMatrix[mid].length == 0) {
-                low = mid + 1;
-                continue;
+            int row = mid / nCols;
+            int col = mid % nCols;
+            if(sortedMatrix[row][col] == target){
+                return new int[]{row,col};
             }
-
-            int nCols = sortedMatrix[mid].length;
-            int first = sortedMatrix[mid][0];
-            int last = sortedMatrix[mid][nCols - 1];
-
-            if (first <= target && target <= last) {
-                // Perform binary search within the row
-                int left = 0;
-                int right = nCols - 1;
-                while (left <= right) {
-                    int colMid = left + (right - left) / 2;
-                    if (sortedMatrix[mid][colMid] == target) {
-                        return new int[]{mid, colMid};
-                    } else if (sortedMatrix[mid][colMid] < target) {
-                        left = colMid + 1;
-                    } else {
-                        right = colMid - 1;
-                    }
-                }
-                break; // Target not found in this row
-            } else if (target < first) {
-                high = mid - 1;
-            } else {
+            if(sortedMatrix[row][col] < target){
                 low = mid + 1;
+            }
+            else{
+                high = mid - 1;
             }
         }
-        return new int[]{-1, -1};
+        return new int[]{-1,-1};
     }
 }
