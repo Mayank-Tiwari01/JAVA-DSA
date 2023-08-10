8c4c6a08f75c65cbd7dcc5c9381f685af56e0557
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-10 12:46:22 +0530

    hwllo

--- BinarySearch2D/SortedMatrix.java
+++ BinarySearch2D/SortedMatrix.java
@@ -7,15 +7,22 @@ public class SortedMatrix {
     public static void main(String[] args){
         int[][] sortedMatrix = {
                 {1,2,3,4,5,6},
-                {7,8},
-                {9,10,11,12}
+                {7,8,9,10,11,12},
+                {13,14,15,16,17,18}
         };
         System.out.println("Enter Element you want to search: ");
         Scanner input = new Scanner(System.in);
         int target = input.nextInt();
-        System.out.println("The element: " +target+ " is at index: " +Arrays.toString(BinarySearch2D(sortedMatrix, target)));
+
+        int[] result = binarySearch2D(sortedMatrix, target);
+        if (result[0] == -1 && result[1] == -1) {
+            System.out.println("Element not found.");
+        } else {
+            System.out.println("The element: " + target + " is at index: " + Arrays.toString(result));
+        }
     }
-    public static int[] BinarySearch2D(int[][]x, int y){
+
+    public static int[] binarySearch2D(int[][]x, int y){
         if( x.length == 0){
             return new int[]{-1,-1};
         }
