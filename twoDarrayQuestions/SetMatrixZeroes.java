553b5cf7112ef1aebdb4c6badedcf82db6e46368
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-13 19:15:48 +0530

    hwllo

--- twoDarrayQuestions/SetMatrixZeroes.java
+++ twoDarrayQuestions/SetMatrixZeroes.java
@@ -1,14 +1,59 @@
 package twoDarrayQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SetMatrixZeroes {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1,1,1},
-                {1,0,1},
-                {1,1,1}
+                {0,1,2,0},
+                {3,4,5,2},
+                {1,3,1,5}
         };
-    }
-    static int[][] SetZeroMatrix(int[][] matrix){
+        int[][] resultMatrix = SetZeroMatrix(matrix);
 
+        for (int[] row : resultMatrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    static int[][] SetZeroMatrix(int[][] matrix) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if(matrix[row][col] == 0){
+                    MarkRow(matrix,row);
+                    MarkCOl(matrix,col);
+                }
+            }
+        }
+        for (int row = 0; row < matrix.length; row++){
+            for (int col = 0; col < matrix[row].length; col++){
+                if(matrix[row][col] == -1){
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+    static int[][] MarkRow(int[][] matrix, int row){
+        int noOfCOls = matrix[0].length;
+        for(int i = 0; i < noOfCOls; i++){
+            if(matrix[row][i] != 0){
+                matrix[row][i] = -1;
+            }
+        }
+        return matrix;
+    }
+    static int[][] MarkCOl(int[][] matrix, int col){
+        int noOfRows = matrix.length;
+
+        for(int j = 0; j < noOfRows; j++){
+            if(matrix[j][col] != 0){
+                matrix[j][col] = -1;
+            }
+        }
+        return matrix;
     }
 }
+
