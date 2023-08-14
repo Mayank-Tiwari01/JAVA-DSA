5f1933b2212db48f24a6556b21d94af3e9ac3ee9
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-14 12:19:14 +0530

    hwllo

---
+++ SortingQuestions/KthLargestElementArray.java
@@ -0,0 +1,16 @@
+package SortingQuestions;
+
+import java.util.Arrays;
+// Min-Heap or QuickSelect Algorithm would be better obv.
+public class KthLargestElementArray {
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.println(KthLargest(nums, k));
+    }
+    static int KthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
+
