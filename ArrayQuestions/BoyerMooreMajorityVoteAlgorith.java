7091127b909d16d2cef904243d4b69a6c58ecff4
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-15 16:02:34 +0530

    hwllo

--- ArrayQuestions/BoyerMooreMajorityVoteAlgorith.java
+++
@@ -1,5 +0,0 @@
-package ArrayQuestions;
-
-public class BoyerMooreMajorityVoteAlgorith {
-
-}
---
+++ ArrayQuestions/BoyerMooreMajorityVoteAlgorithm.java
@@ -0,0 +1,30 @@
+package ArrayQuestions;
+/*
+if the question says there can be or can not be a majority element
+ then we have to check again if the found majorElement occurs more than
+  nums.length / 2 or not. so an extra O(n) step.
+ */
+public class BoyerMooreMajorityVoteAlgorithm {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,2,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1};
+        System.out.println(BoyerMoore(nums));
+    }
+    static int BoyerMoore(int[] nums){
+        int possibleMajor = nums[0];
+        int count = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0){
+                possibleMajor = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == possibleMajor){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return possibleMajor;
+    }
+}
