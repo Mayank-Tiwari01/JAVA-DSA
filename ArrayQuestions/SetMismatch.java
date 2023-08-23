3ec51cbb8914acde819a6d48790865b42472ccd4
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-23 23:46:01 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ ArrayQuestions/SetMismatch.java
@@ -0,0 +1,30 @@
+package ArrayQuestions;
+
+import java.util.Arrays;
+
+public class SetMismatch {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,7,9,10};
+        System.out.println(Arrays.toString(setMismatch(nums)));
+    }
+    static int[] setMismatch(int[] nums) {
+        int[] setMismatch = new int[2];
+
+        for (int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] > 0){
+                nums[index] *= -1;
+            }
+            else if(nums[index] < 0){
+                setMismatch[0] = index + 1;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                setMismatch[1] = i + 1;
+            }
+        }
+        return setMismatch;
+    }
+}
