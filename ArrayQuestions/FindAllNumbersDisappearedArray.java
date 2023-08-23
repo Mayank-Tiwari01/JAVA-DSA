a8297c6e3a001a617c17c614898f77f5e5bed647
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-23 11:14:18 +0530

    Merge remote-tracking branch 'origin/main'

--- ArrayQuestions/FindAllNumbersDisappearedArray.java
+++ ArrayQuestions/FindAllNumbersDisappearedArray.java
@@ -1,5 +1,11 @@
 package ArrayQuestions;
 import java.util.ArrayList;
+/*
+This code is correct, but it is too brute force. It will give TLE error.
+also the code is pretty basic and i took an hour just implementing it
+kind of ashamed of myself. Today is 23 aug 2023.
+ */
+
 public class FindAllNumbersDisappearedArray {
     public static void main(String[] args) {
         int[] nums = {1,1,1,1,1,1,1,2};
