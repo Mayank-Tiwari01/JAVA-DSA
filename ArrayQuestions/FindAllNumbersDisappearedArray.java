c9370a1dd9464ec069cfdebb2216bf1e7ab8c620
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-23 11:12:01 +0530

    Merge remote-tracking branch 'origin/main'

--- ArrayQuestions/FindAllNumbersDisappearedArray.java
+++ ArrayQuestions/FindAllNumbersDisappearedArray.java
@@ -2,14 +2,31 @@ package ArrayQuestions;
 import java.util.ArrayList;
 public class FindAllNumbersDisappearedArray {
     public static void main(String[] args) {
-        int[] nums = {4,3,2,7,8,2,3,1};
+        int[] nums = {1,1,1,1,1,1,1,2};
         System.out.println(missingNums(nums));
     }
-    static ArrayList<Integer> missingNums(int[] nums){
-        ArrayList<Integer> missingNums = new ArrayList<>();
-       int first = 0;
-       int second = 1;
 
+    static ArrayList<Integer> missingNums(int[] nums) {
+        ArrayList<Integer> missingNums = new ArrayList<>();
+        int pointer = 0;
+        boolean found = false;
+
+        for (int i = 1; i <= nums.length; i++) {
+            found = false;
+            pointer = 0;
+            while(pointer < nums.length){
+                if(nums[pointer] != i){
+                    pointer++;
+                }
+                else if(nums[pointer] == i){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                missingNums.add(i);
+            }
+        }
         return missingNums;
     }
 }
