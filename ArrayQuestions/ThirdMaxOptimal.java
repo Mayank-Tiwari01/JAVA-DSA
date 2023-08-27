d5327940aff518192f2246ea2d5518161996a48d
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-08-27 17:12:34 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ ArrayQuestions/ThirdMaxOptimal.java
@@ -0,0 +1,33 @@
+package ArrayQuestions;
+
+public class ThirdMaxOptimal {
+    public static void main(String[] args) {
+        int[] nums = {2,2,3,1};
+        System.out.println(thirdMax(nums));
+    }
+    static int thirdMax(int[] nums) {
+        long firstMax = Long.MIN_VALUE;
+        long secMax = Long.MIN_VALUE;
+        long thirdMax = Long.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > firstMax) {
+                thirdMax = secMax;
+                secMax = firstMax;
+                firstMax = nums[i];
+            }
+            else if (nums[i] > secMax && nums[i] < firstMax) {
+                thirdMax = secMax;
+                secMax = nums[i];
+            }
+            else if (nums[i] > thirdMax && nums[i] < firstMax && nums[i] < secMax) {
+                thirdMax = nums[i];
+            }
+        }
+
+        if (thirdMax != Long.MIN_VALUE) {
+            return (int) thirdMax;
+        }
+        return (int) firstMax;
+    }
+}
