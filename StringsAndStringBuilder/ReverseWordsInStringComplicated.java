a923e6e0eca4b4782122ff63539813acf5db90db
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-09-04 17:52:42 +0530

    Merge remote-tracking branch 'origin/main'

--- StringsAndStringBuilder/ReverseWordsInStringComplicated.java
+++ StringsAndStringBuilder/ReverseWordsInStringComplicated.java
@@ -1,13 +1,14 @@
 package StringsAndStringBuilder;
 
-public class ReverseWordsInStringWithoutExtraSpace {
+public class ReverseWordsInStringComplicated {
     public static void main(String[] args) {
-        String s = "     It is a sunny day   with sunlight and   clouds         ";
+        String s = "    It is a sunny day   with sunlight and   clouds         ";
         System.out.println(reverseWordsInString(s));
     }
     static String reverseWordsInString(String s) {
         int i = 0;
         char[] charArray = s.toCharArray();
+        String ans = "";
 
         while (i < charArray.length) {
 
@@ -21,15 +22,25 @@ public class ReverseWordsInStringWithoutExtraSpace {
             }
             reverseWords(charArray, j, i - 1);
         }
-        String ans = "";
+
+        int numOfSpace = 0;
         for (int j = charArray.length - 1; j >= 0; j--) {
+
             if (charArray[j] != ' ') {
                 ans += charArray[j];
             }
-        }
+            if (charArray[j] == ' ') {
+                int pointer = j;
+                while ( pointer >= 0 && charArray[pointer] == ' ') {
+                    pointer--;
+                }
+                ans += ' ';
+                j = pointer + 1;
+                }
+            }
+        ans = ans.trim();
         return ans;
-    }
-
+        }
     static void reverseWords(char[] charArray, int startIndex, int endIndex) {
         while (startIndex <= endIndex) {
             char temp = charArray[startIndex];
