181e9cfa7a03d0cf668f7d30701d240eef3b0d16
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-09-10 15:11:51 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ StringsAndStringBuilder/PatternSearchingBetterMySol.java
@@ -0,0 +1,51 @@
+package StringsAndStringBuilder;
+/*
+this solution beats 100% of users in java in leetcode (only return the first index in that question)
+also searches for patterns that are different from each other i.e str = abccc and pat = cc would find only 2. and not 2, 3.
+ */
+import java.util.ArrayList;
+
+public class PatternSearchingBetterMySol {
+    public static void main(String[] args) {
+        String str = "abcabcdabcyxabcdabxabcdabcdabcyabcdabcy";
+        String pat = "abcdabcy";
+        System.out.println(searchPattern(str, pat));
+    }
+    static ArrayList<Integer> searchPattern (String str, String pat) {
+        ArrayList <Integer> patternIndex = new ArrayList<>();
+        if (pat.length() == 1) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == pat.charAt(0)) {
+                    patternIndex.add(i);
+                }
+            }
+        }
+        else {
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == pat.charAt(0)) {
+                    if (str.regionMatches(j, pat, 0, pat.length())) {
+                        patternIndex.add(j);
+                    }
+                }
+            }
+        }
+        return patternIndex;
+    }
+}
+/*
+how regionMatches works internally:
+
+It first checks if the specified regions in both strings have the same length.
+If they don't, it returns false immediately because strings with different lengths cannot be equal.
+
+If the lengths match, it iterates over the characters in both regions and compares them character by character,
+starting from the specified positions.
+
+It continues comparing characters until one of the following conditions is met:
+
+All characters in both regions are compared, and they are all equal.
+In this case, it returns true because the specified regions are equal.
+A pair of characters is found to be different. In this case, it returns false because the strings are not equal.
+If it reaches the end of the specified regions without finding any differences,
+it returns true because the regions are equal.
+ */
--- StringsAndStringBuilder/PatternSearchingBruteForce.java
+++ StringsAndStringBuilder/PatternSearchingBruteForce.java
@@ -1,50 +1,29 @@
 package StringsAndStringBuilder;
-/*
-this solution beats 100% of users in java in leetcode (only return the first index in that question)
- */
+//gfg
 import java.util.ArrayList;
 
 public class PatternSearchingBruteForce {
     public static void main(String[] args) {
-        String str = "abcabcdabcyxabcdabxabcdabcdabcyabcdabcy";
-        String pat = "abcdabcy";
+        String str = "abcazzzz";
+        String pat = "zz";
         System.out.println(searchPattern(str, pat));
     }
     static ArrayList<Integer> searchPattern (String str, String pat) {
         ArrayList <Integer> patternIndex = new ArrayList<>();
-        if (pat.length() == 1) {
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == pat.charAt(0)) {
-                    patternIndex.add(i);
+        int strLen = str.length();
+        int patLen = pat.length();
+
+        for (int i = 0; i <= (strLen - patLen); i++) {
+            int j;
+            for (j = 0; j < patLen; j++) {
+                if (pat.charAt(j) != str.charAt(i + j)) {
+                    break;
                 }
             }
-        }
-        else {
-            for (int j = 0; j < str.length(); j++) {
-                if (str.charAt(j) == pat.charAt(0)) {
-                    if (str.regionMatches(j, pat, 0, pat.length())) {
-                        patternIndex.add(j);
-                    }
-                }
+            if (j == patLen) {
+                patternIndex.add(i);
             }
         }
         return patternIndex;
     }
 }
-/*
-how regionMatches works internally:
-
-It first checks if the specified regions in both strings have the same length.
-If they don't, it returns false immediately because strings with different lengths cannot be equal.
-
-If the lengths match, it iterates over the characters in both regions and compares them character by character,
-starting from the specified positions.
-
-It continues comparing characters until one of the following conditions is met:
-
-All characters in both regions are compared, and they are all equal.
-In this case, it returns true because the specified regions are equal.
-A pair of characters is found to be different. In this case, it returns false because the strings are not equal.
-If it reaches the end of the specified regions without finding any differences,
-it returns true because the regions are equal.
- */
