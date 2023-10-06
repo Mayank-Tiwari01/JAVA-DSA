ec28c4cede0dfdc70621aba25fab28ff059168c0
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-06 20:19:51 +0530

    Prefix sum array

---
+++ Mathematics/IntegerBreak.java
@@ -0,0 +1,25 @@
+package Mathematics;
+
+import java.util.*;
+
+public class IntegerBreak {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any Number (Between 2 - 58): ");
+        int n = input.nextInt();
+        System.out.println("The Maximum Product of " + n + " is: " +maxProduct(n));
+        input.close();
+    }
+    static int maxProduct(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int result = 1;
+
+        while (n > 4) {
+            result *= 3;
+            n -= 3;
+        }
+        return result * n;
+    }
+}
