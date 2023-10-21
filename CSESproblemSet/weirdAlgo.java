91ba6d6bf06fcc5758ef16f62a096a4829d5bcc2
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-22 00:44:56 +0530

    updated notes

---
+++ CSESproblemSet/weirdAlgo.java
@@ -0,0 +1,24 @@
+package CSESproblemSet;
+
+import java.util.Scanner;
+
+public class weirdAlgo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long n = input.nextLong();
+        printNums(n);
+    }
+    static void printNums (long n) {
+        while (n != 1) {
+            System.out.print(n + " ");
+            if (n % 2 == 0) {
+                n/= 2;
+            }
+            else {
+                n *= 3;
+                n += 1;
+            }
+        }
+        System.out.println(1);
+    }
+}
