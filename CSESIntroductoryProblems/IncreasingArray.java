0a77c5bb1ba52e2e9724d7573dc4a6d3ef42f848
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-23 17:51:18 +0530

    That is a shot of an emperor.

--- CSESIntroductoryProblems/IncreasingArray.java
+++ CSESIntroductoryProblems/IncreasingArray.java
@@ -1,7 +1,36 @@
 package CSESIntroductoryProblems;
 
+import java.util.Scanner;
+
 public class IncreasingArray {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+        long[] arr = new long[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = input.nextLong();
+        }
+        System.out.println(minOperations(length, arr));
+    }
+    static long minOperations(int length, long[] arr) {
+        if (length == 1) {
+            return 0;
+        }
+        long operations = 0;
+        int first = 0;
+        int second = 0;
 
+        while (second < length) {
+            if (arr[first] < arr[second]) {
+                long sub = arr[second] - arr[first];
+                arr[first] += sub;
+                operations += sub;
+                first++;
+                second++;
+            }
+            first++;
+            second++;
+        }
+        return operations;
     }
 }
