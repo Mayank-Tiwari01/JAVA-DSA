46137d56829d3407e841a3bfbfe663b70a786416
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-25 00:03:28 +0530

    That is a shot of an emperor.

--- Mathematics/twoSCompliment.java
+++ Mathematics/twoSCompliment.java
@@ -5,6 +5,7 @@ public class twoSCompliment {
         int n = 123;
         System.out.println(Integer.toBinaryString(n));
         System.out.println(Integer.toBinaryString(twosCompliment(n)));
+        System.out.println(twosCompliment(n)); // 2's compliments gives us the negative of the number.
     }
     static int twosCompliment(int n) {
         return 1 + ( ~ n );
