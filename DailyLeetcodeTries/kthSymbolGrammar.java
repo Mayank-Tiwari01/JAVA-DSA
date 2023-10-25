dd4573869c459334ceaabbab0a2eca3ac71be237
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-25 20:35:17 +0530

    That is a shot of an emperor.

--- DailyLeetcodeTries/kthSymbolGrammar.java
+++ DailyLeetcodeTries/kthSymbolGrammar.java
@@ -2,19 +2,21 @@ package DailyLeetcodeTries;
 
 public class kthSymbolGrammar {
     public static void main(String[] args) {
-        int n = 2;
-        int k = 2;
+        int n = 5;
+        int k = 4;
+        System.out.println(kThSymbol(n, k));
     }
-    static int kThSymbol(int k) {
-        int[] arr = {0,1};
-        if (k == 1) {
+    static int kThSymbol(int n, int k) {
+        if (k == 1 && n == 1) {
             return 0;
         }
-        else if (k == 2) {
-            return 1;
+        int mid = (int) (Math.pow(2, n - 1) / 2);
+        if (k <= mid) {
+            return kThSymbol(n - 1, k);
         }
         else {
-
+            int symbol = kThSymbol(n-1,k-mid);
+            return symbol == 1 ? 0 : 1;
         }
     }
 }
