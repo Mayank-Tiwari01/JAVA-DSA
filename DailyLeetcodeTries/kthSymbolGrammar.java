6d1181de27f8e371f5c1cf6612754ac710ac1b8d
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-25 13:02:40 +0530

    That is a shot of an emperor.

---
+++ DailyLeetcodeTries/kthSymbolGrammar.java
@@ -0,0 +1,20 @@
+package DailyLeetcodeTries;
+
+public class kthSymbolGrammar {
+    public static void main(String[] args) {
+        int n = 2;
+        int k = 2;
+    }
+    static int kThSymbol(int k) {
+        int[] arr = {0,1};
+        if (k == 1) {
+            return 0;
+        }
+        else if (k == 2) {
+            return 1;
+        }
+        else {
+
+        }
+    }
+}
