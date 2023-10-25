e61e8212a8a2cd9609516a043cfe0d648efe0570
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-26 00:27:27 +0530

    That is a shot of an emperor.

--- Mathematics/isPowerOfTwo.java
+++ Mathematics/isPowerOfTwo.java
@@ -2,13 +2,13 @@ package Mathematics;
 
 class Solution {
     public static void main(String[] args) {
-        int n = 8;
+        int n = 12312;
         System.out.println(isPowerOfTwo(n));
     }
     public static boolean isPowerOfTwo(int n) {
         if (n <= 0) {
             return false;
         }
-        return (n & (n - 1)) == 0;
+        return (n & (n - 1)) == 0; // if there are more than one, 1, then the output won't be zero.
     }
 }
