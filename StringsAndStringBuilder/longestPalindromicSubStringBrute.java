6a12899185b01fa5522aa929901c0c3edf51dd78
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-28 02:33:59 +0530

    That is a shot of an emperor.

---
+++ StringsAndStringBuilder/longestPalindromicSubStringBrute.java
@@ -0,0 +1,41 @@
+package StringsAndStringBuilder;
+/*
+Runtime
+261ms
+Beats 14.80%of users with Java (lol)
+ */
+public class longestPalindromicSubStringBrute {
+    public static void main(String[] args) {
+        String s = "zxcvbnmnbvcxzzxcvbnmnbvcxz";
+        System.out.println(longestSub(s));
+    }
+    static String longestSub(String s) {
+        String longestStr = s.substring(0, 1);
+        int maxStrlen = 0;
+
+        if (s.length() == 1) {
+            return s;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxStrlen; j < s.length(); j++) {
+                if (j - i > maxStrlen && isPalindrome(s, i, j)) {
+                    longestStr = s.substring(i, j + 1);
+                    maxStrlen = j - i;
+                }
+            }
+        }
+        return longestStr;
+    }
+    static boolean isPalindrome(String s, int start, int end) {
+        while (start < end ) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
