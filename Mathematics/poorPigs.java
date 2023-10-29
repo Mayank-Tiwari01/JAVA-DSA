34868918d8e2e6b05ad074e8c18cb6b80a4dc194
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-10-30 01:12:19 +0530

    That is a shot of an emperor.

---
+++ Mathematics/poorPigs.java
@@ -0,0 +1,18 @@
+package Mathematics;
+/*
+https://www.youtube.com/watch?v=YQJLNxveyKg
+ */
+public class poorPigs {
+    public static void main(String[] args) {
+        int buckets = 4, minutesToDie = 15, minutesToTest = 15;
+        System.out.println(minPigs(buckets, minutesToDie, minutesToTest));
+    }
+    static int minPigs(int buckets, int minDie, int minTest) {
+        int rounds = minTest / minDie + 1;
+        int pigs = 0;
+        while (Math.pow(rounds, pigs) < buckets) {
+            pigs++;
+        }
+        return pigs;
+    }
+}
