498f45466b03d48e4aeca5f59f595e3d2965800e
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2023-11-21 13:49:13 +0530

    That is a shot of an emperor.

---
+++ DailyLeetcodeTries/CountNicePairsInArray1814.java
@@ -0,0 +1,13 @@
+package DailyLeetcodeTries;
+/*
+will solve the question later as I have an exam tom.
+But the intuition is clear the second condition will only satisfy when the reverse of the number
+is increase or decreased exactly what the other pair got increased or decreased.
+I hope you got that. so like 13 and 35 is a pair because when you reverse 13 and make it 31 it is
+increased by 18 and when you reverse 35 and make it 53 it is also increased by 18.
+ */
+public class CountNicePairsInArray1814 {
+    public static void main(String[] args) {
+
+    }
+}
