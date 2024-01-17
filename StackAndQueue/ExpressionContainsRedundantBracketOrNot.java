7ff4a2e5cf64fad30e5af7ebdfda5230c134f303
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-01-18 02:49:32 +0530

    2024 is GONNA BE MY YEAR.

--- StackAndQueue/ExpressionContainsRedundantBracketOrNot.java
+++ StackAndQueue/ExpressionContainsRedundantBracketOrNot.java
@@ -3,8 +3,12 @@ import java.util.*;
 /*
 https://www.geeksforgeeks.org/problems/expression-contains-redundant-bracket-or-not/1
 Idea is between every '(' and ')' there should be an operator.
-Whenever you find ')' you start popping because it signifies an end of an expression. And if while popping if you do not
+Whenever you find ')' you start popping because it signifies an end of an expression, And if while popping you do not
 find an operator, then you return false or one.
+I think this code is only good for the gfg problem, cause there weren't many edge cases there, so even if the string
+has something like (*) then also this code will return true,
+and gfg will accept, it says in the question that the expression
+will be valid.
  */
 public class ExpressionContainsRedundantBracketOrNot {
     public static void main(String[] args) {
@@ -12,14 +16,10 @@ public class ExpressionContainsRedundantBracketOrNot {
         System.out.println(checkRedundancy(s));
     }
     static int checkRedundancy(String s) {
-        // code here
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
             if ( c != ')') stack.push(c);
-
-            else if (!stack.empty() && stack.peek() == '(') return 1;
-            else if(stack.empty()) return 1;
             else {
                 boolean isOp = false;
                 while (!stack.empty() && stack.peek() != '(') {
