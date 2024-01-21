1b38a3facbbf437ef6b56062b0c0010f8a70590a
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-01-21 19:20:01 +0530

    2024 is GONNA BE MY YEAR.

--- DailyLeetcodeTries/testCaseGenerator.java
+++ DailyLeetcodeTries/testCaseGenerator.java
@@ -2,7 +2,7 @@ package DailyLeetcodeTries;
 
 public class testCaseGenerator {
     public static void main(String[] args) {
-        for (int i = 1; i <= 2000; i++) {
+        for (int i = 100000; i >= 0 ; i--) {
             System.out.print(i + " ,");
         }
     }
