251d589da7d8c999a7d266e2f6c5e3a9109b7895
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-02-11 16:10:46 +0530

    2024 is GONNA BE MY YEAR.

--- Recursion/MaximizeTheCutSegments.java
+++ Recursion/MaximizeTheCutSegments.java
@@ -1,8 +1,8 @@
 package Recursion;
-
+//TC : O(3^n)
 public class MaximizeTheCutSegments {
     public static void main(String[] args) {
-        System.out.println(maxCut(17, 10, 11, 3));
+        System.out.println(maxCut(170, 10, 11, 140));
     }
     static int maxCut(int n, int x, int y, int z) {
         if (n == 0)
