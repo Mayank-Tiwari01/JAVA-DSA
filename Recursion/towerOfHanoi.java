d2f882dfa7a4315c853e36fe2a55b246c675e6bb
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-02-14 12:26:28 +0530

    2024 is GONNA BE MY YEAR.

--- Recursion/towerOfHanoi.java
+++ Recursion/towerOfHanoi.java
@@ -4,12 +4,12 @@ public class towerOfHanoi {
     public static void main(String[] args) {
         moveDisk(3, 'a', 'b', 'c');
     }
-    static void moveDisk(int n, char a, char b, char c) {
+    static void moveDisk(int n, char from_rod, char to_rod, char aux_rod) {
         if (n == 0)
             return;
 
-        moveDisk(n - 1, a, c, b);
-        System.out.println("Move disk " + n + " from rod " + a + " to rod " + b);
-        moveDisk(n - 1, c, b, a);
+        moveDisk(n - 1, from_rod, aux_rod, to_rod);
+        System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+        moveDisk(n - 1, aux_rod, to_rod, from_rod);
     }
 }
