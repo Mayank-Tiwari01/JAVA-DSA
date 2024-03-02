e67db09cb822859dbc2b16737d482b259766c6a8
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-03-02 11:14:57 +0530

    2024 is GONNA BE MY YEAR.

---
+++ MathematicsAndBitManipulation/SquaresOfASortedArray.java
@@ -0,0 +1,29 @@
+package MathematicsAndBitManipulation;
+/*
+The logic is that the largest number after squaring would either be the first element or the last element.
+Given that the input array is sorted and there may be -ve values.
+ */
+public class SquaresOfASortedArray {
+    public static void main(String[] args) {
+        int[] nums = {-12,2,3,67};
+    }
+    static int[] sortedSquares(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        int idx = len - 1;
+        int start = 0;
+        int end = len - 1;
+
+        while (start <= end) {
+            if (Math.abs(nums[start]) >= Math.abs(nums[end])) {
+                ans[idx--] = nums[start] * nums[start];
+                start++;
+            }
+            else {
+                ans[idx--] = nums[end] * nums[end];
+                end--;
+            }
+        }
+        return ans;
+    }
+}
