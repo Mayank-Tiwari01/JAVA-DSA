407352440f6e3038c097e39ca345987902d15835
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-03-08 18:36:54 +0530

    2024 is GONNA BE MY YEAR.

--- BinaryTrees/MaximumDepthOfBinaryTree.java
+++ BinaryTrees/MaximumDepthOfBinaryTree.java
@@ -1,5 +1,6 @@
 package BinaryTrees;
-//recursive
+import java.util.*;
+//recursive and bfs both. bfs is og.
 public class MaximumDepthOfBinaryTree {
     class TreeNode {
         int val;
@@ -15,12 +16,39 @@ public class MaximumDepthOfBinaryTree {
             this.right = right;
         }
     }
+    //dfs(recursive)
     static int maxDepth(TreeNode root) {
         if (root == null) return 0;
         int maxLeft = maxDepth(root.left);
         int maxRight = maxDepth(root.right);
 
         return 1 + Math.max(maxLeft, maxRight);
+    }
+    static int maxDepthBFS(TreeNode root) {
+        //no need to make it so complicated though, just increment the maxDepth everytime you encounter a new level.
+        //see lc for that code.
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int maxD = 1;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            boolean nextLevel = false;
+            for (int i = 0; i < len; i++) {
+                TreeNode peek = queue.remove();
+                if (peek.left != null) {
+                    queue.add(peek.left);
+                    maxD = (!nextLevel) ? maxD + 1 : maxD;
+                    nextLevel = true;
+                }
+                if (peek.right != null) {
+                    queue.add(peek.right);
+                    maxD = (!nextLevel) ? maxD + 1 : maxD;
+                    nextLevel = true;
+                }
+            }
+        }
+        return maxD;
     }
 
     public static void main(String[] args) {
@@ -33,6 +61,9 @@ public class MaximumDepthOfBinaryTree {
         root.right.left = solution.new TreeNode(4);
         root.right.right = solution.new TreeNode(5);
 
+        Queue<TreeNode> queue = new LinkedList<>();
+
         System.out.println(maxDepth(root));
     }
+
 }
