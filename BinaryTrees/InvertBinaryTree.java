e7f29df895acef09995f43303147c0417bd32761
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-03-19 20:23:00 +0530

    2024 is GONNA BE MY YEAR.

---
+++ BinaryTrees/InvertBinaryTree.java
@@ -0,0 +1,55 @@
+package BinaryTrees;
+import java.util.*;
+public class InvertBinaryTree {
+    class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.data = val;
+            this.left = null;
+            this.right = null;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.data = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //iterative
+    static TreeNode invertTree1(TreeNode root) {
+        if(root == null) return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+
+            if (temp.left != null)
+                queue.add(temp.left);
+            if (temp.right != null)
+                queue.add(temp.right);
+            /*
+            assigning null to a reference variable in Java is a perfectly valid operation and does not cause a NullPointerException. The NullPointerException only occurs when you try to access a method or field of a null object reference.
+            */
+            TreeNode curr = temp.left;
+            temp.left = temp.right;
+            temp.right = curr;
+        }
+        return root;
+    }
+    //recursive
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.right = left;
+        root.left = right;
+
+        return root;
+    }
+}
---
+++ DailyLeetcodeTries/TaskScheduler.java
@@ -0,0 +1,44 @@
+package DailyLeetcodeTries;
+//https://www.youtube.com/watch?v=rYh-Kkbzsnw&list=TLPQMTkwMzIwMjRcfyQ_foGrPw&index=2
+import java.util.*;
+public class TaskScheduler {
+    public static void main(String[] args) {
+        char[] tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+        int n = 2;
+        System.out.println(leastInterval(tasks, n));
+    }
+    static  int leastInterval(char[] tasks, int n) {
+        int[] totalTasks = new int[26];
+        int intervals = 0;
+        for (char c : tasks)
+            totalTasks[c - 'A']++;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < 26; i++) {
+            if (totalTasks[i] > 0) pq.add(totalTasks[i]);
+        }
+
+        while (!pq.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+
+            //every time you can pick n + 1 tasks, cause after that we would either
+            //need a cooling time or need to add the tasks which are available.
+            for (int i = 0; i < n + 1; i++) {
+                if (!pq.isEmpty()) {
+                    int freq = pq.poll();
+                    freq -= 1;
+                    list.add(freq);
+                }
+            }
+            //re-add the tasks that are processed.
+            for (int j : list) {
+                if (j > 0) pq.add(j);
+            }
+            //if we processed all the tasks, that means we did not need any cooling time
+            if (pq.isEmpty()) intervals += list.size();
+            //may have used a cooling time
+            else intervals += n + 1;
+        }
+        return intervals;
+    }
+}
