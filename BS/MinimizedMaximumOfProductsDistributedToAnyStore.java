aa479934cfa4f71b712ae58457262a06a11f2bd3
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-06-13 15:17:51 +0530

    Minimized Maximum Of Products Distributed ToAny Store SOLUTION.

---
+++ BS/MinimizedMaximumOfProductsDistributedToAnyStore.java
@@ -0,0 +1,31 @@
+package BS;
+
+public class MinimizedMaximumOfProductsDistributedToAnyStore {
+    static int minimizedMaximum(int n, int[] quantities) {
+        int start = 1;
+        int end = -1;
+
+        for (int i : quantities) {
+            end = Math.max(i, end);
+        }
+        int ans = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (areStoresFilled(n, quantities, mid)) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else
+                start = mid + 1;
+        }
+        return ans;
+    }
+    static boolean areStoresFilled(int n, int[] quantities, int mid) {
+        for (int i = 0; i < quantities.length; i++) {
+            n -= (int) Math.ceil((double) quantities[i] / mid);
+        }
+        return n >= 0;
+    }
+}
