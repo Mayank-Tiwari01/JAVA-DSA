92deeb5d6e8c48eef2f948ff8e95ed779298b298
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-06-18 14:21:17 +0530

    Minimum Number Of Coins To Be Added and patch array solution

---
+++ SortingQuestions/MostProfitAssigningWork.java
@@ -0,0 +1,29 @@
+package SortingQuestions;
+//optimal for the constraints, also an OG solution.
+public class MostProfitAssigningWork {
+    static int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int maxWorkerDiff = 0;
+
+        for (int i : worker)
+            maxWorkerDiff = Math.max(maxWorkerDiff, i);
+
+        int[] maxProCount = new int[maxWorkerDiff + 1];
+        for (int i = 0; i < difficulty.length; i++) {
+            if (difficulty[i] <= maxWorkerDiff)
+                maxProCount[difficulty[i]] = Math.max(profit[i], maxProCount[difficulty[i]]);
+            //there can be duplicate difficulties with different profits.
+        }
+        int currentMax = 0;
+        for (int i = 1; i <= maxWorkerDiff; i++) {
+            currentMax = Math.max(currentMax, maxProCount[i]);
+            maxProCount[i] = currentMax;
+        }
+
+        int totalProfit = 0;
+        for (int i : worker) {
+            totalProfit += maxProCount[i];
+        }
+
+        return totalProfit;
+    }
+}
